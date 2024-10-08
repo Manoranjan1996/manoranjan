bb7f6e4b7e51929a7e561ab2475e31cccd2af0cc
manoranjan jena <mjena99h@gmail.com>
2024-10-08 23:56:18 +0530

    Add

--- CodeWithHarry/01class/src/App.js
+++ CodeWithHarry/01class/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import About from "./component/About";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
+import React, { useState } from "react";
+
 function App() {
   return (
     <>
-      <Navbar title="textUtil" />
+      <Navbar title="TextUtil" />
       <div className="container">
         {/* <TextForm heading="Enter the text to analyse" /> */}
         <About />
